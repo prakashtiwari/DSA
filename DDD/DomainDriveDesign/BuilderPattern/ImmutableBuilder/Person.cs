a0ca54c82e4e4df3ecd5af172bfadaf9b93f31ae
Prakash Tiwari <prakashtiwari.net@gmail.com>
2024-02-08 22:32:12 +0530

    Immutable class builder

---
+++ DDD/DomainDriveDesign/BuilderPattern/ImmutableBuilder/Person.cs
@@ -0,0 +1,58 @@
+﻿namespace BuilderPattern.ImmutableBuilder
+{
+    public class Person
+    {
+        public string FisrtName { get; private set; }
+        public string LastName { get; private set; }
+        public string MiddleName { get; private set; }
+        public IEnumerable<string> Childrens { get; private set; }
+        public Person(string firstName, string lastName, string middleName, IEnumerable<string> children = null)
+        {
+            this.FisrtName = firstName;
+            this.LastName = lastName;
+            this.MiddleName = middleName;
+            Childrens = children;
+        }
+        public Person(Builder builder)
+        {
+            this.FisrtName = builder.FisrtName;
+            this.LastName = builder.LastName;
+            this.MiddleName = builder.MiddleName;
+            this.Childrens = builder.childrens;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name is {this.FisrtName + " " + " " + this.MiddleName+ " " + this.LastName}");
+        }
+        public class Builder
+        {
+            internal string FisrtName { get; private set; }
+            internal string LastName { get; private set; }
+            internal string MiddleName { get; private set; }
+            internal IEnumerable<string> childrens
+            {
+                get; private set;
+            }
+            public Builder(string fName, string lName)
+            {
+                this.FisrtName = fName;
+                this.LastName = lName;
+            }
+            public Builder AddMiddleName(string name)
+            {
+                MiddleName = name;
+                return this;
+            }
+            public Builder AddChildren(IEnumerable<string> children)
+            {
+                childrens = children;
+                return this;
+            }
+            public Person Build()
+            {
+                return new Person(this);
+            }
+
+        }
+    }
+}
--- DDD/DomainDriveDesign/BuilderPattern/Program.cs
+++ DDD/DomainDriveDesign/BuilderPattern/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern.ImmutableBuilder;
+
 Console.WriteLine("Hello, World!");
+
+Person person = new Person.Builder("Prakash", "Tiwari").AddMiddleName("Prakky").Build();
+person.Show();
